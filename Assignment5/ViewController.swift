89fa7c32cb81736eb95a266c0c8fd499185ce029
Markus <mabar16@student.sdu.dk>
2019-11-12 11:16:53 +0100

    Initial Commit

---
+++ Assignment5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment5
+//
+//  Created by Markus Barrow on 12/11/2019.
+//  Copyright © 2019 University of Southern Denmark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
